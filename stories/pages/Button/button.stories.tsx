28b0aebcfaa818df324a4a693df1dcd0590b983e
Abner André <abner@oialbert.com.br>
2022-09-05 10:06:30 -0300

    teste update

--- stories/pages/Button/button.stories.tsx
+++ stories/pages/Button/button.stories.tsx
@@ -1,9 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
 import Button from "./Button";
+import { Meta } from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react";
 
 export default {
   title: "Pages/Button",
   component: Button,
-};
+} as Meta;
 
-export const ButtonPage = () => <Button />;
+const Template: Story = (args) => <Button {...args} />;
+
+export const DefaultButton = Template.bind({});
+// export const ButtonPage = () => <Button />;
